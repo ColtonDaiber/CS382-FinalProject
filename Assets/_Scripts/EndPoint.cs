a8d3fb090e7d48df290d98c939a6d354bf203a9a
wmarsha <wmarsha@siue.edu>
2025-04-13 17:59:45 -0500

    Updates

--- Assets/_Scripts/EndPoint.cs
+++ Assets/_Scripts/EndPoint.cs
@@ -3,19 +3,15 @@ using Unity.VisualScripting;
 using UnityEngine;
 using UnityEngine.AI;
 
-public class Loot : MonoBehaviour
+public class EndPoint : MonoBehaviour
 {
-    public float worth = 100;
-
-    void OnTriggerEnter(Collider coll){
+        void OnTriggerEnter(Collider coll){
         GameObject otherGO = coll.gameObject;
-        if(otherGO.transform.parent == null) return;
+            if(otherGO.transform.parent == null) return;
         
         Player p = otherGO.transform.parent.GetComponent<Player>();
         if(p != null){
-         Destroy(this.gameObject);
-         p.score += worth;    
+         Destroy(this.gameObject);   
         }
-    }
-
+        }
 }
--- Assets/_Scripts/LevelManager.cs
+++ Assets/_Scripts/LevelManager.cs
@@ -1,4 +1,7 @@
+using System.Collections.Generic;
+using Unity.VisualScripting;
 using UnityEngine;
+using UnityEngine.AI;
 
 public class LevelManager : MonoBehaviour
 {
