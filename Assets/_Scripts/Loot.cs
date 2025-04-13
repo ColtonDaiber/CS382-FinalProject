6b96404a3a7c64f20ec11dcc79760520c5029b7f
wmarsha <wmarsha@siue.edu>
2025-04-13 17:14:17 -0500

    Updates

--- Assets/_Scripts/Loot.cs
+++ Assets/_Scripts/Loot.cs
@@ -4,11 +4,11 @@ public class Loot : MonoBehaviour
 {
     public float worth = 100;
 
-    void OnCollisionEnter(Collision coll){
+    void OnTriggerEnter(Collision coll){
         GameObject otherGO = coll.gameObject;
         Player p = otherGO.GetComponent<Player>();
         if(p != null){
-         Destroy(this);
+         Destroy(this.gameObject);
          p.score += worth;    
         }
     }
