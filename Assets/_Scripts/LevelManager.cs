be1f64954a477fb112eed7fe815ee1f8d2ee86f0
wmarsha <wmarsha@siue.edu>
2025-04-13 19:14:41 -0500

    Alpha Done?
    I think that everything we wanted basic functionality for in the alpha is done

--- Assets/_Scripts/LevelManager.cs
+++ Assets/_Scripts/LevelManager.cs
@@ -2,14 +2,19 @@ using System.Collections.Generic;
 using Unity.VisualScripting;
 using UnityEngine;
 using UnityEngine.AI;
+using TMPro;
 
 public class LevelManager : MonoBehaviour
 {
  public float minLevelScore = 500;
+ [SerializeField] TextMeshProUGUI text;
  [SerializeField] Player playerGO;
 void Update(){
  if(playerGO.score >= minLevelScore){
     playerGO.canLeave = true;
         }
+    text.text = "Score: " + playerGO.score.ToString() + " of " + minLevelScore.ToString();
     }
+
+
 }
